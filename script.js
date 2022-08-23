6b6c7e2b2e06b0519fb9f546651494217999fb58
Splzats <jacoblahaye@gmail.com>
2022-08-23 16:22:59 -0400

    add reset and resolution controls

--- script.js
+++ script.js
@@ -1,31 +1,63 @@
-let canvasSize = 16;
 const canvas = document.querySelector("main");
+const resetBtn = document.querySelector("#reset");
+const resizeInput = document.querySelector("#resizeInput");
+const resizeBtn = document.querySelector("#resizeBtn");
+
+let canvasSize = 16;
+
+resizeInput.addEventListener("input", function () {
+  canvasSize = resizeInput.valueAsNumber;
+  console.log(canvasSize);
+});
+
+resizeBtn.addEventListener("click", function () {
+  console.log(canvasSize);
+  if (canvasSize >= 1 && canvasSize <= 100) {
+    reset();
+    createGrid(canvasSize);
+    createBlocks(canvasSize ** 2);
+
+    const pixels = canvas.querySelectorAll(".pixel");
+    for (i = 0; i < pixels.length; i++) {
+      pixels[i].addEventListener("mouseover", function (e) {
+        e.target.style.background = "red";
+      });
+    }
+  } else {
+    alert("Invalid input, enter a value between 1 and 100.");
+  }
+});
+
+resetBtn.addEventListener("click", reset);
 
 function createBlocks(num) {
   let blockArray = [];
   for (let i = 0; i < num; i++) {
     blockArray[i] = document.createElement("div");
-    blockArray[i].setAttribute(
-      "style",
-      "background: blue; height: 75px; width: 100%;"
-    );
+    blockArray[i].setAttribute("style", "background: #fff");
     blockArray[i].classList.add("pixel");
     canvas.appendChild(blockArray[i]);
   }
 }
 
-function createGrid() {
+function createGrid(size) {
   canvas.setAttribute(
     "style",
-    `gap: 0; grid-template-columns: repeat(${canvasSize}, 1fr); grid-template-rows: repeat(${canvasSize}, 1fr);`
+    `gap: 0; grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`
   );
 }
 
-createGrid();
+function reset() {
+  const pixels = canvas.querySelectorAll(".pixel");
+  for (let i = 0; i < pixels.length; i++) {
+    pixels[i].setAttribute("style", "background: #fff");
+  }
+}
+
+createGrid(canvasSize);
 createBlocks(canvasSize ** 2);
 
 const pixels = canvas.querySelectorAll(".pixel");
-console.log(pixels[1]);
 for (i = 0; i < pixels.length; i++) {
   pixels[i].addEventListener("mouseover", function (e) {
     e.target.style.background = "red";
