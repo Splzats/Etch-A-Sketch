561312617a588d6f7f668a6a592aaedb9ca49697
Splzats <jacoblahaye@gmail.com>
2022-08-23 01:41:03 -0400

    add hover functionality

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ function createBlocks(num) {
   let blockArray = [];
   for (let i = 0; i < num; i++) {
     blockArray[i] = document.createElement("div");
-    blockArray[i].setAttribute("style", "background: blue; height: 100px;");
+    blockArray[i].setAttribute(
+      "style",
+      "background: blue; height: 75px; width: 100%;"
+    );
+    blockArray[i].classList.add("pixel");
     canvas.appendChild(blockArray[i]);
   }
 }
@@ -13,22 +17,17 @@ function createBlocks(num) {
 function createGrid() {
   canvas.setAttribute(
     "style",
-    `display: grid; grid-template-columns: repeat(${canvasSize}, 1fr); grid-template-rows: repeat(${canvasSize}, 1fr);`
+    `gap: 0; grid-template-columns: repeat(${canvasSize}, 1fr); grid-template-rows: repeat(${canvasSize}, 1fr);`
   );
 }
 
 createGrid();
 createBlocks(canvasSize ** 2);
 
-// function renderBlocks(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-//     canvas.appendChild(arr[i]);
-//   }
-// }
-
-// renderBlocks(createBlocks(5));
-
-//  const block = document.createElement("div");
-//   block.setAttribute("style", "background: blue; height: 100px");
-//   canvas.appendChild(block);
+const pixels = canvas.querySelectorAll(".pixel");
+console.log(pixels[1]);
+for (i = 0; i < pixels.length; i++) {
+  pixels[i].addEventListener("mouseover", function (e) {
+    e.target.style.background = "red";
+  });
+}
